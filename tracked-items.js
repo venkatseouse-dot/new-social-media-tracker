e9613648a4834a25c33d9974c3a15666d38dd4c0
venkatseouse-dot <venkatseouse@gmail.com>
2025-12-03 13:19:02 +0530

    Update tracked-items.js

--- tracked-items.js
+++ tracked-items.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const { method } = req;
   const userId = req.query.userId || "default_user";
 
